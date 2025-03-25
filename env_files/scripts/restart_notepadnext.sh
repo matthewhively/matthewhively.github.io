4b9af43869dcabd23dd8334c7bd150d426bc99ed
Matthew Hively <matthewhively@viz.com>
2025-03-24 23:36:02 -0700

    Added cron-ish script to regularlly restart NotepadNext
    This will help prevent losing too much unsaved changes

---
+++ env_files/scripts/restart_notepadnext.sh
@@ -0,0 +1,26 @@
+#!/bin/bash 
+
+# Script called by plist file:
+#    ~/Library/LaunchAgents/com.matt.notepadnext.plist
+#
+# Cron-enabled by:  launchctl load   ~/Library/LaunchAgents/com.matt.notepadnext.plist 
+# Cron-disabled by: launchctl unload ~/Library/LaunchAgents/com.matt.notepadnext.plist 
+
+
+echo "Refreshing NotepadNext to save session windows @ $(date)"
+
+# Close the app (gracefully)
+osascript -e 'quit app "NotepadNext"'
+# NOTE: gives error msg "0:22: execution error: NotepadNext got an error: User canceled. (-128)"
+
+# Apparently using pkill (TERM or QUIT), doesn't seem to do it gracefully (maybe a different signal would work)
+
+sleep 5
+
+
+# Re-Open the App
+open ~/Applications/NotepadNext.app
+
+echo "Completed @ $(date)"
+echo
+echo
